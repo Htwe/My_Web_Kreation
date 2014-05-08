994283eda1920daa77d9abe45fba0b59f820a549
KhineWin Htwe <khinewinhtwe@gmail.com>
2014-05-08 21:49:27 +0800

    Product lists pages

---
+++ Assignment3_NewDesign/gloves.php
@@ -0,0 +1,433 @@
+<html>
+<head>
+<title>Yes I Do | Accessories</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link href="css/style.css" rel="stylesheet" type="text/css" />
+<?php
+	session_start();
+?>
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
+<script type="text/javascript" src="js/jquery.bxslider.min.js"></script>
+<script type="text/javascript">
+function Slider(){
+	$(".slider #1").show("fade",500);
+	$(".slider #1").delay(5500).hide("slide",{direction:"left"},500);
+			var sc = $(".slider img").size();
+			var count=2;
+			setInterval(function(){
+				$(".slider #"+count).show("slide",{direction:"right"},500); 
+				$(".slider #"+count).delay(5500).hide("slide",{direction:"left"},500);
+				if(count == sc){
+					count = 1;
+				}else{
+					count = count+1;
+				}
+			},6500);
+}
+</script>
+<script>
+    function submitForm(id, action)
+    {
+		if (id == '6' ) 
+		{
+			document.getElementById('product6').action = action;
+			document.getElementById('product6').submit();
+		} else if (id == '5' ) 
+		{
+			document.getElementById('product5').action = action;
+			document.getElementById('product5').submit();
+		}
+		else if (id == '4' ) 
+		{
+			document.getElementById('product4').action = action;
+			document.getElementById('product4').submit();
+		}
+		else if (id == '3' ) 
+		{
+			document.getElementById('product3').action = action;
+			document.getElementById('product3').submit();
+		}
+		else if (id == '2' ) 
+		{
+			document.getElementById('product2').action = action;
+			document.getElementById('product2').submit();
+		}
+		else if (id == '1' ) 
+		{
+			document.getElementById('product').action = action;
+			document.getElementById('product').submit();
+		}		
+    }
+</script>
+<script>
+    function btnSubmitForm(action)
+    {
+        document.getElementById('btnSubmit').action = action;
+        document.getElementById('btnSubmit').submit();
+    }
+</script>
+</head>
+
+<body onLoad="Slider();">
+<div class="main">
+	<!---Header--->
+	<div class="header">
+		<div class="header_resize">
+			<div class="customer_connect">
+				<table>
+					<tr>
+						<td style="width: 100px; padding-left: 300px; "><a href="#" style="text-decoration: none;">Facebook</a></td>
+						<td style="width: 100px; text-align:center;"><a href="#" style="text-decoration: none;">Twitter</a></td>
+						<td style="width: 100px; text-align:center;"><a href="#" style="text-decoration: none;">Google+</a></td>
+						<td style="width: 100px; padding-left:400px;"><?php 
+								if(isset($_SESSION['Username'])){
+								echo($_SESSION['Username']."</a>&nbsp; <a href = 'logout.php' style='text-decoration: none'>Log Out</a>");
+								}else {
+								echo ("<a href='login.php' style='text-decoration: none;'>Login</a>");
+								}
+						?></td>
+						<td style="width: 100px;"><a href="registration.php" style="text-decoration: none;">Register</a></td>
+					</tr>
+				</table>
+			</div>
+            <div class="logobg">
+			<div class="logo">
+				<a href="index.php"><img src="image/logo.png" width="130" height="100" alt=""/></a>
+			</div>			
+			<div class="menu_nav">
+				<ul>
+				  <li class="active"><a href="index.php"><span><strong>Home</strong></span></a>
+                 <ul>
+						<li><a href="weddingD.php"><strong>Wedding Dresses</strong></a>
+						<ul>
+							<li><a href="weddingdress.php"><strong>Wedding Dresses</strong></a></li>
+							<li><a href="informaldress.php"><strong>Informal Dresses</strong></a></li>
+							<li><a href="bridesmaiddress.php"><strong>Bridesmaid Dresses</strong></a></li>
+							<li><a href="quinceaneradress.php"><strong>Quinceanera Dresses</strong></a></li>                            
+							<li><a href="promdress.php"><strong>Prom Dresses</strong></a></li>
+						</ul>
+						</li>
+						<li><a href="#"><strong>Shoes</strong></a>
+						<ul>
+							<li><a href="#"><strong>Evening Shoes</strong></a></li>
+							<li><a href="#"><strong>Day Shoes</strong></a></li>
+							<li><a href="#"><strong>Bridal Shoes</strong></a></li>
+						</ul>
+						</li>
+						<li><a href="#"><strong>Accessories</strong></a>
+						<ul>
+							<li><a href="veils.php"><strong>Veils</strong></a></li>
+							<li><a href="headpieces.php"><strong>Headpieces</strong></a></li>
+							<li><a href="jewellery.php"><strong>Jewellery</strong></a></li>
+							<li><a href="gloves.php"><strong>Gloves</strong></a></li>
+							<li><a href="garter.php"><strong>Garter</strong></a></li>
+						</ul>
+						</li>
+
+					</ul>
+					</li>				  			  
+				  <li class="active"><a href="#"><span><strong>Knowledge</strong></span></a>
+					<ul>
+						<li><a href="sizeChart.php"><strong>Size Chart</strong></a></li>
+						<li><a href="colorChart.php"><strong>Color Chart</strong></a></li>
+						<li><a href="faq.php"><strong>FAQ</strong></a></li>
+						<li><a href="terms.php"><strong>Terms&Conditions</strong></a></li>
+					</ul>
+				  </li>					
+				  <li><a href="#"><span><strong>Service</strong></span></a>
+					<ul>
+						<li><a href="delivery_reference.php"><strong>Delivery</strong></a></li>
+						<li><a href="payment_reference.php"><strong>Credit Cart Payment</strong></a></li>						
+					</ul>
+				  </li>
+				  <li><a href="#"><span><strong>About</strong></span></a>
+					<ul>
+						<li><a href="About_us.php"><strong>About Us</strong></a></li>
+						<li><a href="Contact_us.php"><strong>Contact Us</strong></a></li>						
+					</ul>
+				  </li>
+				</ul>
+			</div>
+			<div class="clr"></div>
+            </div>
+		</div>
+	</div>
+	
+	<!---Content--->
+	<div class="content">
+		<div class="content_resize">
+			<div class="shopping_card">
+				<form id="btnSubmit" action="#">				
+				<?php 
+				if(isset($_SESSION['Username'])){
+					$connect = mysql_connect("localhost","root","");
+					mysql_select_db("yesido_db");
+					$myusername = $_SESSION['Username'];
+					$query = mysql_query("SELECT * FROM yesido_db.sc WHERE Username = '$myusername' ");
+					$totalCost = 0;
+					//fetch the results 
+					WHILE($rows = mysql_fetch_array($query)):
+					$proCode = $rows['ProCode'];
+					$proQty = $rows['ProQty'];
+					$proPrice = $rows['ProPrice'];
+					$totalCost += $proQty * $proPrice;
+					endwhile;
+					if ($totalCost > 0) 
+					{
+						$query = mysql_query("SELECT * FROM yesido_db.sc WHERE Username = '$myusername' ");
+						WHILE($rows = mysql_fetch_array($query)):
+						$proCode = $rows['ProCode'];
+						$proQty = $rows['ProQty'];
+						$proPrice = $rows['ProPrice'];
+						echo "
+						<h3>P Code: $proCode<h3> 
+						<h3>P Qty: $proQty</h3>
+						<h3>Price: $proPrice</h3>
+						<p>--------------------</p>
+						";
+						endwhile;
+					
+					echo "						
+						Total Cost &nbsp; &nbsp; <input type='text' name='Total' size='10' value='$totalCost' placeholder='S$0.00' readonly /><br /><br />
+							 <input type='button' value='CHECK OUT' name='Submit' class='btncheckout' onclick=\"btnSubmitForm('delivery.php')\" />
+							 <input type='button' value='Empty' name='Submit' class='btnEmpty' onclick=\"btnSubmitForm('delete.php')\" />
+						";					
+					}
+					else {				
+						echo "
+							<label> No Product has been added</label>
+								<p>-------------------------------------------------</p>							   
+							  Total Cost &nbsp; &nbsp; <input type='text' name='Total' size='10' placeholder='S$0.00' readonly /><br /><br />
+							 <input type='button' value='CHECK OUT' name='Submit' class='btncheckout' onclick=\"btnSubmitForm('delivery.php')\" />
+							 <input type='button' value='Empty' name='Submit' class='btnEmpty' onclick=\"btnSubmitForm('delete.php')\" />
+						";
+					}									
+				}
+				else {				
+						echo "
+							<label> No Product has been added</label>
+								<p>-------------------------------------------------</p>							   
+							  Total Cost &nbsp; &nbsp; <input type='text' name='Total' size='10' placeholder='S$0.00' readonly /><br /><br />
+							 <input type='button' value='CHECK OUT' name='Submit' class='btncheckout' onclick=\"btnSubmitForm('delivery.php')\" />
+							 <input type='button' value='Empty' name='Submit' class='btnEmpty' onclick=\"btnSubmitForm('delete.php')\" />
+						";
+					}
+				?>
+                
+                </form>
+			</div>
+			
+			<div class="main_text">
+				<h1>Gloves</h1>
+				<div class="product_catalog">
+					<table class="product_lists">
+						<tr>
+							<td style="height: 450px; text-align:center;">							
+								<p>
+									<a class="image_text" href="product67.php" title="Price = S$58">
+									<img src="image/Accessories/Gloves/glove_1.jpg" width="280px" height="400px" border="3"/>
+									</a>
+								</p>								
+							</td>
+							<td style="height: 450px; text-align:center;">
+								<p>
+									<a class="image_text" href="product68.php" title="Price = S$65">
+									<img src="image/Accessories/Gloves/glove_2.jpg" width="280px" height="400px" border="3"/>
+									</a>
+								</p>
+							</td>
+							<td style="height: 450px; text-align:center;">
+								<p>
+									<a class="image_text" href="product69.php" title="Price = S$45">
+									<img src="image/Accessories/Gloves/glove_3.jpg" width="280px" height="400px" border="3"/>
+									</a>
+								</p>
+							</td>
+						</tr>
+						<tr>
+							<td class="product_details">
+								<form id="product" name="product" action="productFunction.php" method="post">
+									<input type="hidden" name="ProCode" value="P0067">	
+									<input type="hidden" name="ProPrice" value="58">
+									<input type="hidden" name="ProDesc" value="Bridal Rhinestone Gloves, Formal Lace Gloves">
+									<input type="hidden" name="ProImageName" value="glove_1.jpg">
+									<input type="hidden" name="quantity" value="1">
+																		
+									<input type="button" id="detailsBtn" name="product" onclick="submitForm('1', 'product67.php')" value="Details" />
+									<input type="button" id="addChartBtn" name="product" onclick="submitForm('1','productFunction.php')" value="Add Cart" />									
+								</form>
+							</td>
+							<td class="product_details">
+								<form  id="product2" name="product2" action="productFunction.php" method="post">
+									<input type="hidden" name="ProCode" value="P0068">	
+									<input type="hidden" name="ProPrice" value="65">
+									<input type="hidden" name="ProDesc" value="Bridal Gloves, Long Traditional Wedding Gloves">
+									<input type="hidden" name="ProImageName" value="glove_2.jpg">
+									<input type="hidden" name="quantity" value="1">
+								
+									<input type="button" id="detailsBtn" name="product2" onclick="submitForm('2', 'product68.php')" value="Details" />
+									<input type="button" id="addChartBtn" name="product2" onclick="submitForm('2','productFunction.php')" value="Add Cart" />
+								</form>
+							</td>
+							<td class="product_details">
+								<form id="product3" name="product3" action="productFunction.php" method="post">
+									<input type="hidden" name="ProCode" value="P0069">	
+									<input type="hidden" name="ProPrice" value="45">
+									<input type="hidden" name="ProDesc" value="Ivory Lace Bridal gloves">
+									<input type="hidden" name="ProImageName" value="glove_3.jpg">
+									<input type="hidden" name="quantity" value="1">
+									
+									<input type="button" id="detailsBtn" name="product3" onclick="submitForm('3', 'product69.php')" value="Details" />
+									<input type="button" id="addChartBtn" name="product3" onclick="submitForm('3','productFunction.php')" value="Add Cart" />
+								</form>
+							</td>
+						</tr>
+                        <tr>
+							<td style="height: 450px; text-align:center;">							
+								<p>
+									<a class="image_text" href="product70.php" title="Price = S$45">
+									<img src="image/Accessories/Gloves/glove_4.jpg" width="280px" height="400px" border="3"/>
+									</a>
+								</p>								
+							</td>
+							<td style="height: 450px; text-align:center;">
+								<p>
+									<a class="image_text" href="product71.php" title="Price = S$88">
+									<img src="image/Accessories/Gloves/glove_5.jpg" width="280px" height="400px" border="3"/>
+									</a>
+								</p>
+							</td>
+							<td style="height: 450px; text-align:center;">
+								<p>
+									<a class="image_text" href="product72.php" title="Price = S$43">
+									<img src="image/Accessories/Gloves/glove_6.jpg" width="280px" height="400px" border="3"/>
+									</a>
+								</p>
+							</td>
+						</tr>
+						<tr>
+							<td class="product_details">
+								<form  id="product4" name="product4" action="productFunction.php" method="post">
+									<input type="hidden" name="ProCode" value="P0070">	
+									<input type="hidden" name="ProPrice" value="45">
+									<input type="hidden" name="ProDesc" value="Bridal Victorian lace cuffs bridal cuffs wedding gloves">
+									<input type="hidden" name="ProImageName" value="glove_4.jpg">
+									<input type="hidden" name="quantity" value="1">
+								
+									<input type="button" id="detailsBtn" name="product4" onclick="submitForm('4', 'product70.php')" value="Details" />
+									<input type="button" id="addChartBtn" name="product4" onclick="submitForm('4','productFunction.php')" value="Add Cart" />
+								</form>
+							</td>
+							<td class="product_details">
+								<form  id="product5" name="product5" action="productFunction.php" method="post">
+									<input type="hidden" name="ProCode" value="P0071">	
+									<input type="hidden" name="ProPrice" value="88">
+									<input type="hidden" name="ProDesc" value="Wedding Gloves, Ivory Ribbon Gloves">
+									<input type="hidden" name="ProImageName" value="glove_5.jpg">
+									<input type="hidden" name="quantity" value="1">
+								
+									<input type="button" id="detailsBtn" name="product5" onclick="submitForm('5', 'product71.php')" value="Details" />
+									<input type="button" id="addChartBtn" name="product5" onclick="submitForm('5','productFunction.php')" value="Add Cart" />
+								</form>
+							</td>
+							<td class="product_details">
+								<form  id="product6" name="product6" action="productFunction.php" method="post">
+									<input type="hidden" name="ProCode" value="P0072">	
+									<input type="hidden" name="ProPrice" value="43">
+									<input type="hidden" name="ProDesc" value="Bridal Gloves Lace Gloves">
+									<input type="hidden" name="ProImageName" value="glove_6.jpg">
+									<input type="hidden" name="quantity" value="1">
+								
+									<input type="button" id="detailsBtn" name="product6" onclick="submitForm('6', 'product72.php')" value="Details" />
+									<input type="button" id="addChartBtn" name="product6" onclick="submitForm('6','productFunction.php')" value="Add Cart" />
+								</form>
+							</td>
+						</tr>
+					</table>
+					<br/>
+					
+				</div>
+				<div class="clr"></div>
+			</div>
+			</div>
+			<div class="clr"></div>				
+			</div>
+			<div class="clr"></div>
+		</div>
+	</div>
+	
+	<!---FooterBg--->
+	<div class="footerBg">
+		<div class="footerBg_resize">
+			<div class="column c1">
+				<h2><span>Photo Gallery</span></h2>
+				<a href="photo_gallery.php"><img src="image/gallery/gallery1.jpg" width="75" height="75" alt="" class="gal" /></a> 
+				<a href="photo_gallery.php"><img src="image/gallery/gallery2.jpg" width="75" height="75" alt="" class="gal" /></a> 
+				<a href="photo_gallery.php"><img src="image/gallery/gallery3.jpg" width="75" height="75" alt="" class="gal" /></a> 
+				<a href="photo_gallery.php"><img src="image/gallery/gallery4.jpg" width="75" height="75" alt="" class="gal" /></a> 
+				<a href="photo_gallery.php"><img src="image/gallery/gallery5.jpg" width="75" height="75" alt="" class="gal" /></a> 
+				<a href="photo_gallery.php"><img src="image/gallery/gallery6.jpg" width="75" height="75" alt="" class="gal" /></a> 
+			</div>
+			<div class="column c2">
+				<h2><span>Feedback</span></h2>
+					<form method="post" name="feedbackFrom" action="">						
+						<input name="name" type="text" placeholder="Name" class="name-bg" required>	<p></p>				
+						<input name="email" type="text" placeholder="E-mail" class="email-bg" required>	<p></p>
+						<!---
+						<textarea cols="42" rows="42" placeholder="comments" class="comment-bg">Leave your comments here.</textarea>
+						--->
+						<input name="comment" type="text" placeholder="Leave your comments here." class="comment-bg" required> <p></p>
+						<input class="submitBtn" type="submit" value="Submit">						
+					</form>
+			</div>
+			<div class="column c3">				
+				<h2><span>Testimonials</span></h2>
+					<div class="testimonials-slider">
+						<div class="slide">							
+								<div class="testimonials-carousel-context">
+                                <div class="testimonials-name">Ivory</div>
+                                <div class="testimonials-carousel-content"><p>This is the most useful website for me.</p></div>
+                            </div>
+						</div>
+                      
+						<div class="slide">							
+								<div class="testimonials-carousel-context">
+								<div class="testimonials-name">Janet</div>
+								<div class="testimonials-carousel-content"><p>I have found this website help me a lot in finding my suitable wedding dress and accessories.</p></div>
+							</div>
+						</div>
+                      
+						<div class="slide">							
+								<div class="testimonials-carousel-context">
+                                <div class="testimonials-name">Kathy</div>
+                                <div class="testimonials-carousel-content"><p>I like this website design and the products production.</p></div>
+                            </div>
+						</div>
+					</div>				
+			</div>
+			<div class="clr"></div>
+		</div>
+	</div>
+	
+	<!--Footer--->
+	<div class="footer">
+		<div class="footer_resize">
+			<p class="lf">Copyright &copy; MyWebKreation. All Rights Reserved.</p>
+		</div>
+	</div>
+</div>
+<!--jQuery plug-in code-->
+<!---<script>
+	$('.testimonials-slider').bxSlider({
+		slideWidth: 296,
+		minSlides: 1,
+		maxSlides: 1,
+		slideMargin: 32,
+		auto: true,	
+		autoControls: true
+	  });
+</script> --->
+</body>
+
+</html>
