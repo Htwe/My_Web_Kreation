5b5d0a34876db82590d5c1fda95d4cae571f9de1
skyheart007 <samyao.sdm93@gmail.com>
2014-05-10 02:21:57 +0800

    login_fixed

---
+++ login.php
@@ -0,0 +1,340 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<?php
+	if(!isset($_SESSION)){
+		session_start();
+	}
+?>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Yes I Do | login</title>
+
+<link href="css/style.css" rel="stylesheet" type="text/css" />
+
+<script type="text/javascript">
+function Slider(){
+	$(".slider #1").show("fade",500);
+	$(".slider #1").delay(5500).hide("slide",{direction:"left"},500);
+			var sc = $(".slider img").size();
+			var count=2;
+			setInterval(function(){
+				$(".slider #"+count).show("slide",{direction:"right"},500); 
+				$(".slider #"+count).delay(5500).hide("slide",{direction:"left"},500);
+				if(count == sc){
+					count = 1;
+				}else{
+					count = count+1;
+				}
+			},6500);
+}
+function validForm(form){
+	if(form.Username.value = ""){
+		alert("Error: Username cannot be blank!");
+		form.Username.focus();
+		return false;
+	}
+	var Username = /^[0-9a-zA-Z]+$/;
+	if(!Username.test(form.Username.value)){
+		alert('Username should be contain only numbers and alphabets');
+		form.Username.focus();
+		return false;
+	}
+	if(form.Pass.value == ""){
+		alert("Enter your password!!");
+		form.Pass.focus();
+		return false;
+	}
+	if(form.Pass.value.length < 6){
+		alert("Password must contain at least six characters!");
+		form.Pass.focus();
+		return false;
+	}
+	psw = /^[0-9a-zA-Z]+$/;
+	if(!psw.test(form.Pass.value)){
+		alert('Password should be contain only numbers and alphabets!');
+		form.Username.focus();
+		return false;
+	}
+	if(form.Pass.value != form.CPass.value){
+		alert("Password do not match");
+		return false;
+	}
+	
+	//var email = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	if(!email.test(form.Email.value)){
+		alert('Enter the correct Email Type!');
+		form.Email.focus();
+		return false;
+	}
+	
+	var PhNo = /^[0-9]/;
+	if(!PhNo.test(form.ContactNo.value)){
+		alert('Enter the correct Contact Number');
+		form.Contact.focus();
+		return false;
+	}
+return true;
+}
+</script>
+<script>
+    function submitForm(action)
+    {
+        document.getElementById('btnSubmit').action = action;
+        document.getElementById('btnSubmit').submit();
+    }
+</script>
+
+</head>
+
+<body onLoad="Slider();">
+<div class="main">
+	<!---Header--->
+	<div class="header">
+		<div class="header_resize">
+			<div class="customer_connect">
+				<table>
+					<tr>
+						<td style="width: 100px; padding-left: 300px; "><a href="https://www.facebook.com/yesidoweddingdressjcu" style="text-decoration: none;"><img src="image/Logos/facebook.png"/></a></td>
+						<td style="width: 100px; text-align:center;"><a href="#" style="text-decoration: none;"><img src="image/Logos/twitter.png"/></a></td>
+						<td style="width: 100px; text-align:center;"><a href="#" style="text-decoration: none;"><img src="image/Logos/google.png"/></a></td>
+                        <td style="width: 200px; padding-left:400px;"><?php 
+								if(isset($_SESSION['Username'])){
+								echo($_SESSION['Username']."</a>&nbsp; <a href = 'logout.php' style='text-decoration: none'><img src='image/Logos/Logout.png'/></a>");
+								}else {
+								echo ("<a href='login.php' style='text-decoration: none;'>Login</a><a href='registration.php' style='text-decoration: none;'><img src='image/Logos/Register.png'/></a>");
+								}
+						?></td>
+					</tr>
+				</table>
+			</div>
+			<div class="logobg">
+			<div class="logo">
+				<a href="index.php"><img src="image/logo.png" width="130" height="100" alt=""/></a>
+			</div>			
+			<div class="menu_nav">
+				<ul>
+				  <li class="active"><a href="index.php"><span><strong>Home</strong></span></a>
+                  <ul>
+						<li><a href="weddingD.php"><strong>Wedding Dresses</strong></a>
+						<ul>
+							<li><a href="weddingdress.php"><strong>Wedding Dresses</strong></a></li>
+							<li><a href="informaldress.php"><strong>Informal Dresses</strong></a></li>
+							<li><a href="bridesmaiddress.php"><strong>Bridesmaid Dresses</strong></a></li>
+							<li><a href="quinceaneradress.php"><strong>Quinceanera Dresses</strong></a></li>                            
+							<li><a href="promdress.php"><strong>Prom Dresses</strong></a></li>
+						</ul>
+						</li>
+						<li><a href="#"><strong>Shoes</strong></a>
+						<ul>
+							<li><a href="eveningshoes.php"><strong>Evening Shoes</strong></a></li>
+							<li><a href="dayshoes.php"><strong>Day Shoes</strong></a></li>
+							<li><a href="bridalshoes.php"><strong>Bridal Shoes</strong></a></li>
+						</ul>
+						</li>
+						<li><a href="#"><strong>Accessories</strong></a>
+						<ul>
+							<li><a href="veils.php"><strong>Veils</strong></a></li>
+							<li><a href="headpieces.php"><strong>Headpieces</strong></a></li>
+							<li><a href="jewellery.php"><strong>Jewellery</strong></a></li>
+							<li><a href="gloves.php"><strong>Gloves</strong></a></li>
+							<li><a href="garter.php"><strong>Garter</strong></a></li>
+						</ul>
+						</li>
+
+					</ul>
+					</li>				  
+				  <li class="active"><a href="#"><span><strong>Knowledge</strong></span></a>
+					<ul>
+						<li><a href="sizeChart.php"><strong>Size Chart</strong></a></li>
+						<li><a href="colorChart.php"><strong>Color Chart</strong></a></li>
+						<li><a href="faq.php"><strong>FAQ</strong></a></li>
+						<li><a href="terms.php"><strong>Terms&Conditions</strong></a></li>
+					</ul>
+				  </li>					
+				  <li><a href="#"><span><strong>Service</strong></span></a>
+					<ul>
+						<li><a href="delivery_reference.php"><strong>Delivery</strong></a></li>
+						<li><a href="payment_reference.php"><strong>Credit Cart Payment</strong></a></li>						
+					</ul>
+				  </li>
+				  <li><a href="#"><span><strong>About</strong></span></a>
+					<ul>
+						<li><a href="About_us.php"><strong>About Us</strong></a></li>
+						<li><a href="Contact_us.php"><strong>Contact Us</strong></a></li>						
+					</ul>
+				  </li>
+				</ul>
+			</div>
+			
+			<div class="clr"></div>
+            </div>
+		</div>
+	</div>
+	
+	<!---Content--->
+	<div class="content">
+		<div class="content_resize">
+			<div class="shopping_card">
+				<form id="btnSubmit" action="#">				
+				<?php 
+				if(isset($_SESSION['Username'])){
+					$connect = mysql_connect("localhost","root","");
+					mysql_select_db("yesido_db");
+					$myusername = $_SESSION['Username'];
+					$query = mysql_query("SELECT * FROM yesido_db.sc WHERE Username = '$myusername' ");
+					$totalCost = 0;
+					//fetch the results 
+					WHILE($rows = mysql_fetch_array($query)):
+					$proCode = $rows['ProCode'];
+					$proQty = $rows['ProQty'];
+					$proPrice = $rows['ProPrice'];
+					$totalCost += $proQty * $proPrice;
+					endwhile;
+					if ($totalCost > 0) 
+					{
+						$query = mysql_query("SELECT * FROM yesido_db.sc WHERE Username = '$myusername' ");
+						WHILE($rows = mysql_fetch_array($query)):
+						$proCode = $rows['ProCode'];
+						$proQty = $rows['ProQty'];
+						$proPrice = $rows['ProPrice'];
+						echo "
+						<h3>P Code: $proCode<h3> 
+						<h3>P Qty: $proQty</h3>
+						<h3>Price: $proPrice</h3>
+						<p>--------------------</p>
+						";
+						endwhile;
+					
+					echo "						
+						Total Cost &nbsp; &nbsp; <input type='text' name='Total' size='10' value='$totalCost' placeholder='S$0.00' readonly /><br /><br />
+							 <input type='button' value='CHECK OUT' name='Submit' class='btncheckout' onclick=\"submitForm('delivery.php')\" />
+							 <input type='button' value='Empty' name='Submit' class='btnEmpty' onclick=\"submitForm('delete.php')\" />
+						";					
+					}
+					else {				
+						echo "
+							<label> No Product has been added</label>
+								<p>-------------------------------------------------</p>							   
+							  Total Cost &nbsp; &nbsp; <input type='text' name='Total' size='10' placeholder='S$0.00' readonly /><br /><br />
+							 <input type='button' value='CHECK OUT' name='Submit' class='btncheckout' onclick=\"submitForm('delivery.php')\" />
+							 <input type='button' value='Empty' name='Submit' class='btnEmpty' onclick=\"submitForm('delete.php')\" />
+						";
+					}									
+				}
+				else {				
+						echo "
+							<label> No Product has been added</label>
+								<p>-------------------------------------------------</p>							   
+							  Total Cost &nbsp; &nbsp; <input type='text' name='Total' size='10' placeholder='S$0.00' readonly /><br /><br />
+							 <input type='button' value='CHECK OUT' name='Submit' class='btncheckout' onclick=\"submitForm('delivery.php')\" />
+							 <input type='button' value='Empty' name='Submit' class='btnEmpty' onclick=\"submitForm('delete.php')\" />
+						";
+					}
+				?>
+                
+                </form>
+			</div>
+			
+            <div class="main_text">	
+            <br />
+            <div class="formSection">
+            <form name='login' action="userlogin.php" method="post" >
+				<fieldset>
+					<legend>Sign in</legend>
+					<br />
+					<p>
+					<label for="Username">Username</label> 
+					<input type="text" name="Username" size="15" placeholder="Enter Username" title="please enter correct Username"/> 
+					</p>
+					<p>
+					<label for="password">Password</label>
+					<input type="password" name="Pass" size="15" placeholder = "Enter Password" required="required" />
+					</p>
+					<p>
+					<input type="submit" name="login" value="Sign in" /> 
+					</p>
+				</fieldset>
+			</form>
+
+ </div>
+<div class="clr"></div>
+			</div>
+			</div>
+			<div class="clr"></div>				
+			</div>
+			<div class="clr"></div>
+		</div>
+	</div>
+    	<!---FooterBg--->
+	<div class="footerBg">
+		<div class="footerBg_resize">
+			<div class="column c1">
+				<h2><span>Photo Gallery</span></h2>
+				<a href="photo_gallery.php"><img src="image/gallery/gallery1.jpg" width="75" height="75" alt="" class="gal" /></a> 
+				<a href="photo_gallery.php"><img src="image/gallery/gallery2.jpg" width="75" height="75" alt="" class="gal" /></a> 
+				<a href="photo_gallery.php"><img src="image/gallery/gallery3.jpg" width="75" height="75" alt="" class="gal" /></a> 
+				<a href="photo_gallery.php"><img src="image/gallery/gallery4.jpg" width="75" height="75" alt="" class="gal" /></a> 
+				<a href="photo_gallery.php"><img src="image/gallery/gallery5.jpg" width="75" height="75" alt="" class="gal" /></a> 
+				<a href="photo_gallery.php"><img src="image/gallery/gallery6.jpg" width="75" height="75" alt="" class="gal" /></a> 
+			</div>
+			<div class="column c2">
+				<h2><span>Feedback</span></h2>
+					<form method="post" name="feedbackFrom" action="">						
+						<input name="name" type="text" placeholder="Name" class="name-bg" required>	<p></p>				
+						<input name="email" type="text" placeholder="E-mail" class="email-bg" required>	<p></p>
+						<!---
+						<textarea cols="42" rows="42" placeholder="comments" class="comment-bg">Leave your comments here.</textarea>
+						--->
+						<input name="comment" type="text" placeholder="Leave your comments here." class="comment-bg" required> <p></p>
+						<input class="submitBtn" type="submit" value="Submit">						
+					</form>
+			</div>
+			<div class="column c3">				
+				<h2><span>Testimonials</span></h2>
+					<div class="testimonials-slider">
+						<div class="slide">							
+								<div class="testimonials-carousel-context">
+                                <div class="testimonials-name">Ivory</div>
+                                <div class="testimonials-carousel-content"><p>This is the most useful website for me.</p></div>
+                            </div>
+						</div>
+                      
+						<div class="slide">							
+								<div class="testimonials-carousel-context">
+								<div class="testimonials-name">Janet</div>
+								<div class="testimonials-carousel-content"><p>I have found this website help me a lot in finding my suitable wedding dress and accessories.</p></div>
+							</div>
+						</div>
+                      
+						<div class="slide">							
+								<div class="testimonials-carousel-context">
+                                <div class="testimonials-name">Kathy</div>
+                                <div class="testimonials-carousel-content"><p>I like this website design and the products production.</p></div>
+                            </div>
+						</div>
+					</div>				
+			</div>
+			<div class="clr"></div>
+		</div>
+	</div>
+	
+	<!--Footer--->
+	<div class="footer">
+		<div class="footer_resize">
+			<p class="lf">Copyright &copy; MyWebKreation. All Rights Reserved.</p>
+		</div>
+	</div>
+</div>
+<!--jQuery plug-in code-->
+<!-- <script>
+	$('.testimonials-slider').bxSlider({
+		slideWidth: 296,
+		minSlides: 1,
+		maxSlides: 1,
+		slideMargin: 32,
+		auto: true,	
+		autoControls: true
+	  });
+</script> -->
+</body>
+</html>
